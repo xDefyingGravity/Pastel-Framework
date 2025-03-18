2d04210cf2ea7fa1622e000a3d11557092196bc4
Will Ballantine <defying.gravity711@gmail.com>
2025-03-18 14:00:07 -0400

    Changes

--- utils/build.ts
+++ utils/build.ts
@@ -1,13 +1,9 @@
-import dts from 'bun-plugin-dts'
-
-// @ts-ignore
 await Bun.build({
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
   format: 'esm',
-  external: ['expr-eval', 'node:path', 'bun-plugin-dts'],
+  external: ['expr-eval', 'node:path'],
   minify: true,
   sourcemap: 'external',
   target: 'browser',
-  plugins: [dts()],
 })
